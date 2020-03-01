592252d358299473dd4ba18056a2c763cb5b4f36
MrJack <biagio.toby@hotmail.it>
2020-03-01 17:40:02 +0100

    Added these method into Ingredient's model: - getNotDeletedCookies; - getDeletedCookies; - isVeggie; - isVegan; - isDeleted. Refactored from getCookies to getAllCookies

--- app/Ingredient.php
+++ app/Ingredient.php
@@ -10,9 +10,33 @@ class Ingredient extends Model
     protected $table = 'ingredients';
     protected $primaryKey = 'id';
 
-    public function getCookies()
+    public function getAllCookies()
     {
         return $this->belongsToMany('App\Cookie','cookie_recipes');
     }
 
+    public function getNotDeletedCookies()
+    {
+        return  $this->getAllCookies()->where('is_deleted',false);
+    }
+
+    public function getDeletedCookies()
+    {
+        return  $this->getAllCookies()->where('is_deleted',true);
+    }
+
+    public function isVeggie()
+    {
+        return $this->is_veggie;
+    }
+
+    public function isVegan()
+    {
+        return $this->is_vegan;
+    }
+
+    public function isDeleted()
+    {
+        return $this->is_deleted;
+    }
 }
