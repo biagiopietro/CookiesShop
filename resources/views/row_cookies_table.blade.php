0ecc73d1aa9dba396fd03c26e9ba862444e98dfd
MrJack <biagio.toby@hotmail.it>
2020-03-04 11:28:49 +0100

    Added hover to the cookies table's rows

--- resources/views/row_cookies_table.blade.php
+++ resources/views/row_cookies_table.blade.php
@@ -1,4 +1,4 @@
-<tr class="alternate-color">
+<tr class="alternate-color hover:bg-gray">
     <td class="px-4 py-2">{{ $cookie->name }}</td>
     <td class="text-center px-4 py-2">{{ $cookie->weight }}</td>
     <td class="text-center px-4 py-2">{{ $cookie->calories }}</td>
