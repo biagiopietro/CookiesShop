0a63473f76f879f5bf4451b1a17272d0fd745aad
MrJack <biagio.toby@hotmail.it>
2020-03-04 11:08:14 +0100

    Aligned to center the weight and the calories fields of the cookies

--- resources/views/row_cookies_table.blade.php
+++ resources/views/row_cookies_table.blade.php
@@ -1,6 +1,6 @@
 <tr class="alternate-color">
     <td class="px-4 py-2">{{ $cookie->name }}</td>
-    <td class="px-4 py-2">{{ $cookie->weight }}</td>
-    <td class="px-4 py-2">{{ $cookie->calories }}</td>
+    <td class="text-center px-4 py-2">{{ $cookie->weight }}</td>
+    <td class="text-center px-4 py-2">{{ $cookie->calories }}</td>
 </tr>
 
