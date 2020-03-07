474f05c73c4bfa4c191c1b9da500e2109d82b795
MrJack <biagio.toby@hotmail.it>
2020-03-07 17:51:15 +0100

    Added ingredients.blade.php

---
+++ resources/views/ingredients.blade.php
@@ -0,0 +1,11 @@
+@extends('welcome')
+
+@section('content')
+    <div class="flex mb-4">
+        <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2"></div>
+        <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2">
+            <ingredients-section></ingredients-section>
+        </div>
+        <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2 bg-"></div>
+    </div>
+@endsection
