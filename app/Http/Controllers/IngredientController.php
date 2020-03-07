6851761b63c4646eff37e7fa9f38aebdaaaf6fad
MrJack <biagio.toby@hotmail.it>
2020-03-07 17:51:44 +0100

    Added ingredients controller

---
+++ app/Http/Controllers/IngredientController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Ingredient;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class IngredientController extends Controller
+{
+    //
+    public function index(Request $request)
+    {
+        // If the request has header `Accept: */json`, return JSON
+        if ($request->wantsJson()) {
+            return $this->search($request);
+        }
+        return view('ingredients', ['ingredients' => $this->paginateIngredient(Ingredient::query())]);
+    }
+
+    private function search(Request $request)
+    {
+        if ($request->filled('search')) {
+            $search = $request->search;
+            $filteredIngredients = Ingredient::where('name', 'LIKE', '%' . $search . '%');
+        } else {
+            $filteredIngredients = Ingredient::query();
+
+        }
+        return $this->paginateIngredient($filteredIngredients);
+    }
+
+    private function paginateIngredient($ingredients)
+    {
+        return response()->json($ingredients->paginate(10));
+    }
+}
