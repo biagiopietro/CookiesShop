657c14ea24854423ff7028f3a37b29f31b036ca5
MrJack <biagio.toby@hotmail.it>
2020-03-09 17:47:47 +0100

    Added assertOrderingColumnSort() method to the CustomDuskBrowser

--- tests/Browser/CustomDuskBrowser.php
+++ tests/Browser/CustomDuskBrowser.php
@@ -4,6 +4,7 @@ namespace Tests\Browser;
 
 use Laravel\Dusk\Browser;
 use PHPUnit\Framework\Assert as PHPUnit;
+use Symfony\Component\DomCrawler\Crawler;
 
 class CustomDuskBrowser extends Browser
 {
@@ -16,6 +17,27 @@ class CustomDuskBrowser extends Browser
     public function seeInLastElement($selector, $text, $crawler)
     {
         PHPUnit::assertStringContainsString($text, trim($crawler->filter($selector)->last()->text()));
+        return $this;
+    }
+
+    public function assertOrderingColumnSort($tableSelector,
+                                             $rowSelector,
+                                             $columnSelector,
+                                             $textFirstRow,
+                                             $textLastRow) {
+        $html = $this->element($tableSelector)->getAttribute('innerHTML');
+        $crawler = new Crawler($html);
+
+        $this->seeInFirstElement($rowSelector, $textFirstRow, $crawler);
+        $this->seeInLastElement($rowSelector, $textLastRow, $crawler);
+
+        $this->click($columnSelector);
+
+        $html = $this->element($tableSelector)->getAttribute('innerHTML');
+        $crawler = new Crawler($html);
+
+        $this->seeInFirstElement($rowSelector, $textLastRow, $crawler);
+        $this->seeInLastElement($rowSelector, $textFirstRow, $crawler);
         return $this;
     }
 }
