ac71c05ac77825e8cba63417c842d4e70490ba59
MrJack <biagio.toby@hotmail.it>
2020-03-09 17:04:42 +0100

    Added tooltip when user hover on order icon

--- resources/js/components/OrderIcon.vue
+++ resources/js/components/OrderIcon.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="tooltip">
         <div v-if="getColumnCurrentSortDir() !== 'asc' && getColumnCurrentSort() === this.currentSort">
             <div v-if="!isColumnNumbered">
                 <svg style="width:16px;height:16px" viewBox="0 0 24 24">
@@ -23,8 +23,8 @@
                     <path fill="currentColor" d="M7 11H3V9H7V8H5C3.9 8 3 7.11 3 6V5C3 3.9 3.9 3 5 3H7C8.11 3 9 3.9 9 5V9C9 10.11 8.11 11 7 11M7 5H5V6H7M5 13H7C8.11 13 9 13.9 9 15V19C9 20.11 8.11 21 7 21H5C3.9 21 3 20.11 3 19V15C3 13.9 3.9 13 5 13M5 19H7V15H5M12 5H22V7H12M12 19V17H22V19M12 11H22V13H12Z" />
                 </svg>
             </div>
-
         </div>
+        <span class="tooltiptext">{{ $t('tooltip.sort_current_page_result', {ordering: getAscDescStringFormatted()})}}</span>
     </div>
 </template>
 
@@ -42,6 +42,13 @@
             }
         },
         methods: {
+            getAscDescStringFormatted: function() {
+                if (this.getColumnCurrentSortDir() === 'asc') {
+                    return this.$t('tooltip.descending');
+                }
+                return this.$t('tooltip.ascending');
+
+            },
             getColumnCurrentSort: function() {
                 return this.$parent.getColumnCurrentSort();
             },
--- resources/js/components/VeganVeggieBadge.vue
+++ resources/js/components/VeganVeggieBadge.vue
@@ -2,15 +2,15 @@
     <span>
         <button :id="'cookie-veggie-' + cookie_id" v-if="is_veggie" class="tooltip text-white rounded-6 bg-green-900 w-6 h-6 z-10">
             v
-            <span class="tooltiptext">{{ $t('ingredients_table.is_veggie')}}</span>
+            <span class="tooltiptext">{{ $t('tooltip.veggie')}}</span>
         </button>
         <button :id="'cookie-vegan-' + cookie_id" v-if="is_veggie && is_vegan" class="tooltip text-white rounded-6 bg-cyan w-6 h-6 ml--2 z-0">
             v
-            <span class="tooltiptext">{{ $t('ingredients_table.is_vegan')}}</span>
+            <span class="tooltiptext">{{ $t('tooltip.vegan')}}</span>
         </button>
         <button :id="'cookie-vegan-' + cookie_id" v-else-if="is_vegan" class="tooltip text-white rounded-6 bg-cyan w-6 h-6 z-0">
             v
-            <span class="tooltiptext">{{ $t('ingredients_table.is_vegan')}}</span>
+            <span class="tooltiptext">{{ $t('tooltip.vegan')}}</span>
         </button>
     </span>
 </template>
