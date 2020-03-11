4228e7f34e3549764014f34234cab7b64cd73a43
MrJack <biagio.toby@hotmail.it>
2020-03-11 12:34:30 +0100

    Updated TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,29 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function getHostAndPort()
+    {
+        return "http://localhost:8000";
+    }
+
+    public function getIngredientsUrl()
+    {
+        return '/ingredients';
+    }
+
+    public function getIngredientsFullUrl()
+    {
+        return $this->getHostAndPort().$this->getIngredientsUrl();
+    }
+
+    public function getCookiesUrl()
+    {
+        return '/cookies';
+    }
+
+    public function getCookiesFullUrl()
+    {
+        return $this->getHostAndPort().$this->getCookiesUrl();
+    }
 }
